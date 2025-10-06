8da09f2e693e3c891edb591241128e40a1424fd5
PorpanTH <porpan151@gmail.com>
2025-10-06 09:31:48 +0000

    working sample ingestion + update .yml

--- microservices/ingestor/main.py
+++ microservices/ingestor/main.py
@@ -3,7 +3,8 @@
 if __name__ == "__main__":
 	# In a real deployment, this would be triggered by a cron job.
 	# For our simulation, we can just run it once or in a loop.
-	
+	print("Hello worlds")
+
 	# generate our Ingestor instances
 	# cron job run for all (multi thread each ingestor)
 	pass
