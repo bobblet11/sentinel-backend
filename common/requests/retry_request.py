64afd4473396ac1481acc7dc47812938c4ba14ca
bobblet11 <benglover78@gmail.com>
2025-10-21 03:05:46 +0000

    fix retry core

--- common/requests/retry_request.py
+++ common/requests/retry_request.py
@@ -1,33 +1,44 @@
+import time
+from math import e
+from random import uniform
+
 # make this a function wrapper
 # something like this idk
-def retry(max_attempts = 3, delay_ms = 1000, action, **args, **kwargs): 
-	for attempt in range(1, max_attempts+1):
-		try:
-                	print(f"Attempt #{attempt}")
-			return action(args, kwargs)
+def retry(max_attempts = 3, delay_s = 1): 
+	def decorator_retry(func):
+		def wrapper(*args, **kwargs):
+			for attempt in range(1, max_attempts+1):
+				try:
+					print(f"Attempt #{attempt}")
+					return func(*args, **kwargs)
+				except Exception as e:
+					if attempt == max_attempts:
+						print(f"Fatal failure at attempt {attempt}. Exiting...")
+						raise e
 
-		except Exception as e:
-			if attempt == max_attempts:
-				print(f"Fatal failure at attempt {attempt}. Exiting...")
-				raise e
+					print(f"Failure at attempt {attempt}. Will retry in {delay_s} ms")
+					time.sleep(delay_s)
+		return wrapper
+	return decorator_retry
+     
+     
+def exponential_retry(max_attempts = 3, initial_delay_s = 1, growth_modifier = 1, growth_rate = 1, jitter=True): 
+	def decorator_exponential_retry(func):
+		def wrapper(*args, **kwargs):
+			for attempt in range(1, max_attempts+1):
+				try:
+					print(f"Attempt #{attempt}")
+					return func(*args, **kwargs)
+				except Exception as e:
+					if attempt == max_attempts:
+						print(f"Fatal failure at attempt {attempt}. Exiting...")
+						raise e
 
-			print(f"Failure at attempt {attempt}. Will retry in {delay_ms} ms")
-			time.sleep(delay_ms)
-   
+					time_to_wait_s =  (growth_modifier * pow(e, attempt * growth_rate)) + initial_delay_s
+					if jitter:
+						time_to_wait_s = uniform(0, time_to_wait_s)
 
-def exponential_retry(max_attempts = 3, initial_delay_ms = 1000, growth_modifier = 1, growth_rate = 1, action, **args, **kwargs): 
-	for attempt in range(1, max_attempts+1):
-		try:
-                	print(f"Attempt #{attempt}")
-			return action(args, kwargs)
-
-		except Exception as e:
-			if attempt == max_attempts:
-				print(f"Fatal failure at attempt {attempt}. Exiting...")
-				raise e
-
-			time_to_wait = (growth_modifier * pow(2.17, attempt * growth_rate)) + initial_delay_ms
-			print(f"Failure at attempt {attempt}. Will retry in {time_to_wait} ms")
-			time.sleep(time_to_wait)
-			
-			
+					print(f"Failure at attempt {attempt}. Will retry in {time_to_wait_s} ms")
+					time.sleep(time_to_wait_s)
+		return wrapper
+	return decorator_exponential_retry
