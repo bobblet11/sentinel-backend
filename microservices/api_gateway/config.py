4b5977332c9e9779ea34c2fab81330eb0c6549b7
Sadiq <sad.iqsalam2004@gmail.com>
2025-10-21 11:40:37 +0000

    features/api-gateway: add FastAPI gateway with async Redis cache and docker-compose entry

---
+++ microservices/api_gateway/config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+REDIS_HOST = os.getenv("REDIS_HOST", "redis")
+REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+WEB_SCRAPER_URL = os.getenv("WEB_SCRAPER_URL", "http://web-scraper:8000")
+NLP_URL = os.getenv("NLP_URL", "http://nlp:8000")
+CACHE_TTL = int(os.getenv("CACHE_TTL", 3600))
+HTTP_TIMEOUT = int(os.getenv("HTTP_TIMEOUT", 15))
+REDIS_URL = f"redis://{REDIS_HOST}:{REDIS_PORT}/0"
