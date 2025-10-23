3a5b11b3455428258eef522b841d7becf8f4daf1
bobblet11 <benglover78@gmail.com>
2025-10-23 10:39:20 +0000

    cron job fixed

--- microservices/ingestor/main.py
+++ microservices/ingestor/main.py
@@ -1,8 +1,12 @@
 import json
+import os
 from microservices.ingestor.rss_ingestor import RssIngestor
 
 if __name__ == "__main__":
-        with open('./rss_feeds.json', 'r') as file:
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        json_file_path = os.path.join(script_dir, 'rss_feeds.json')
+                
+        with open(json_file_path, 'r') as file:
                 rss_feeds = json.load(file)
         rss_ingestor = RssIngestor(rss_feeds) 
         rss_ingestor.run()
