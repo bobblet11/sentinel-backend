f6ea6b074dd7ce730cbe4877fe6a945922130eca
bobblet11 <benglover78@gmail.com>
2025-10-27 09:23:27 +0000

    more printing options

--- common/io/redirect_and_modify.py
+++ common/io/redirect_and_modify.py
@@ -1,12 +1,20 @@
 import io
-from contextlib import redirect_stdout
 import sys
+from contextlib import redirect_stdout
 
-def indent_with_tab(line):
-        return f"\t{line}" 
-def indent_with_space(line):
-        return f"        {line}" 
 
+def indent_with_tab(s):
+        return f"\t{s}" 
+def indent_with_space(s):
+        return f"        {s}" 
+
+def prRed(s): return "\033[91m {}\033[00m".format(s)
+def prGreen(s): return "\033[92m {}\033[00m".format(s)
+def prYellow(s): return "\033[93m {}\033[00m".format(s)
+def prLightPurple(s): return"\033[94m {}\033[00m".format(s)
+def prPurple(s): return "\033[95m {}\033[00m".format(s)
+def prCyan(s): return "\033[96m {}\033[00m".format(s)
+def prLightGray(s): return "\033[97m {}\033[00m".format(s)
 
 def redirect_and_modify(string_modification_function = indent_with_tab):
         def decorator(func):
