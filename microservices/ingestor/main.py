763cb85a0dc16f650155d72d3b35eb5f49e8e70c
bobblet11 <benglover78@gmail.com>
2025-10-27 21:45:32 +0000

    added monitoring to ingestor

--- microservices/ingestor/main.py
+++ microservices/ingestor/main.py
@@ -1,10 +1,13 @@
 import json
 import os
 import datetime
-from common.io.redirect_and_modify import redirect_and_modify, indent_with_space
 from microservices.ingestor.rss_ingestor import RssIngestor
+from common.process.monitor import format_sys_stats, get_sys_stats
+from common.io.redirect_and_modify import redirect_and_modify
+from common.io.utils import indent_with_tab
 
-@redirect_and_modify(string_modification_function=indent_with_space)
+
+@redirect_and_modify(string_modification_function=indent_with_tab)
 def exec():
         script_dir = os.path.dirname(os.path.abspath(__file__))
         json_file_path = os.path.join(script_dir, 'rss_feeds.json')
@@ -15,10 +18,9 @@ def exec():
         rss_ingestor = RssIngestor(rss_feeds) 
         rss_ingestor.run()
 
-if __name__ == "__main__":
-        
-        print(f"\n\nmain.py is being run. It is currently {datetime.datetime.now()}")
-        
-        exec()
 
+if __name__ == "__main__":
+        print(f"\n\nmain.py is being run. It is currently {datetime.datetime.now()}")
+        print(format_sys_stats(get_sys_stats()))
+        exec()
         print(f"\n\nmain.py is finished. It is currently {datetime.datetime.now()}")
