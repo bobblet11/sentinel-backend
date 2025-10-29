08a6109a5bf1584b42734aa770b90c8362e52c03
bobblet11 <u3596276@connect.hku.hk>
2025-10-29 16:39:45 +0000

    formating and linting

--- common/requests/retry_request.py
+++ common/requests/retry_request.py
@@ -2,43 +2,60 @@ import time
 from math import exp
 from random import uniform
 
+
 # make this a function wrapper
 # something like this idk
-def retry(max_attempts = 3, delay_s = 1): 
-	def decorator_retry(func):
-		def wrapper(*args, **kwargs):
-			for attempt in range(1, max_attempts+1):
-				try:
-					print(f"Attempt #{attempt}")
-					return func(*args, **kwargs)
-				except Exception as e:
-					if attempt == max_attempts:
-						print(f"Fatal failure at attempt {attempt}. Exiting...")
-						raise e
+def retry(max_attempts=3, delay_s=1):
+    def decorator_retry(func):
+        def wrapper(*args, **kwargs):
+            for attempt in range(1, max_attempts + 1):
+                try:
+                    print(f"Attempt #{attempt}")
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    if attempt == max_attempts:
+                        print(f"Fatal failure at attempt {attempt}. Exiting...")
+                        raise e
 
-					print(f"Failure at attempt {attempt}. Will retry in {delay_s:.2f} s")
-					time.sleep(delay_s)
-		return wrapper
-	return decorator_retry
-     
-     
-def exponential_retry(max_attempts:int = 3, initial_delay_s:float = 1, growth_modifier:float = 1, growth_rate:float = 1, jitter=False): 
-	def decorator_exponential_retry(func):
-		def wrapper(*args, **kwargs):
-			for attempt in range(1, max_attempts+1):
-				try:
-					print(f"Attempt #{attempt}")
-					return func(*args, **kwargs)
-				except Exception as e:
-					if attempt == max_attempts:
-						print(f"Fatal failure at attempt {attempt}. Exiting...")
-						raise e
-					
-					time_to_wait_s =  (growth_modifier * exp(attempt * growth_rate)) + initial_delay_s
-					if jitter:
-						time_to_wait_s = uniform(time_to_wait_s-1, time_to_wait_s+1)
+                    print(
+                        f"Failure at attempt {attempt}. Will retry in {delay_s:.2f} s"
+                    )
+                    time.sleep(delay_s)
 
-					print(f"Failure at attempt {attempt}. Will retry in {time_to_wait_s:.2f} s")
-					time.sleep(time_to_wait_s)
-		return wrapper
-	return decorator_exponential_retry
+        return wrapper
+
+    return decorator_retry
+
+
+def exponential_retry(
+    max_attempts: int = 3,
+    initial_delay_s: float = 1,
+    growth_modifier: float = 1,
+    growth_rate: float = 1,
+    jitter=False,
+):
+    def decorator_exponential_retry(func):
+        def wrapper(*args, **kwargs):
+            for attempt in range(1, max_attempts + 1):
+                try:
+                    print(f"Attempt #{attempt}")
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    if attempt == max_attempts:
+                        print(f"Fatal failure at attempt {attempt}. Exiting...")
+                        raise e
+
+                    time_to_wait_s = (
+                        growth_modifier * exp(attempt * growth_rate)
+                    ) + initial_delay_s
+                    if jitter:
+                        time_to_wait_s = uniform(time_to_wait_s - 1, time_to_wait_s + 1)
+
+                    print(
+                        f"Failure at attempt {attempt}. Will retry in {time_to_wait_s:.2f} s"
+                    )
+                    time.sleep(time_to_wait_s)
+
+        return wrapper
+
+    return decorator_exponential_retry
