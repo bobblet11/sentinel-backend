ab24c38896be5499662446f6a75677240a0518f0
bobblet11 <u3596276@connect.hku.hk>
2025-10-30 10:25:42 +0000

    cleaned up common for postgres

--- common/models/__init__.py
+++ common/models/__init__.py
@@ -1,5 +1,1 @@
 version = "0.0.1"
-
-from .redis_models import Message, MessageHeader, MessageURLPayload
-
-__all__ = ["MessageHeader", "MessageURLPayload", "Message"]
---
+++ common/models/database/redis_models.py
@@ -0,0 +1,30 @@
+from typing import Any, Union
+
+from pydantic import BaseModel
+
+
+class MessageHeader(BaseModel):
+    """
+    Represents the basic information used to identify and get stats on Messages
+    """
+
+    message_id: str
+    timestamp: str
+
+
+class MessageURLPayload(BaseModel):
+    """
+    Represents the payload between ingestor and web scraper service
+    """
+
+    url: str
+    source_rss: str
+
+
+class Message(BaseModel):
+    """
+    Represents the actual message data type passed through a message queue
+    """
+
+    header: MessageHeader
+    data: Union[MessageURLPayload, Any]  # Fixed for Python 3.9
