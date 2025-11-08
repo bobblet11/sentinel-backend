78204ada85e1c04cce70b2e611e1020aa856fa10
bobblet11 <u3596276@connect.hku.hk>
2025-11-08 16:00:38 +0000

    base setup for web-scraper

--- microservices/web-scraper/main.py
+++ microservices/web-scraper/main.py
@@ -10,7 +10,5 @@ def exec():
 if __name__ == "__main__":
         
         print(f"\n\nmain.py is being run. It is currently {datetime.datetime.now()}")
-        
         exec()
-
         print(f"\n\nmain.py is finished. It is currently {datetime.datetime.now()}")
