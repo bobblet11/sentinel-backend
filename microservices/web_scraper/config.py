af339eb5b60a8e105fdb79bae9a1238fbc3ea779
bobblet11 <u3596276@connect.hku.hk>
2025-11-09 17:45:32 +0000

    added config

---
+++ microservices/web_scraper/config.py
@@ -0,0 +1,20 @@
+import os
+import sys
+from dotenv import load_dotenv
+
+def print_env(PROXIFLY_API_KEY):
+    print(f"Proxifly API key {PROXIFLY_API_KEY[:5]}************** ")
+
+
+load_dotenv()
+
+PROXIFLY_API_KEY = os.getenv("PROXIFLY_API_KEY")
+if not PROXIFLY_API_KEY:
+    print("FATAL: PROXIFLY_API_KEY environment variable is not set. Exiting.")
+    sys.exit(1)
+    
+
+
+print_env(PROXIFLY_API_KEY)
+    
+
