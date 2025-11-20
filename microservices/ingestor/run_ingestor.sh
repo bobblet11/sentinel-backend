ac778ce5b9ad7eb51252d1cb57738bd59f17db63
bobblet11 <u3596276@connect.hku.hk>
2025-11-20 07:22:18 +0000

    new scripts to fix docker tagging issue

--- microservices/ingestor/run_ingestor.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Go to the correct directory
-cd /app
-
-# Run the python script.
-# 'exec' replaces the shell process with the python process.
-exec /usr/local/bin/python3 -m microservices.ingestor.main
