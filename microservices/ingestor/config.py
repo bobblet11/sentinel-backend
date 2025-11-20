a5bcee350ad94a3a5bcdb51ec93251198071dcd2
bobblet11 <u3596276@connect.hku.hk>
2025-11-20 07:42:46 +0000

    ingestor finally uses env properly

--- microservices/ingestor/config.py
+++ microservices/ingestor/config.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from typing import Optional
+from dotenv import load_dotenv
 
 def print_env(
     REDIS_DUPLICATE_FILTER_KEY: str,
@@ -14,15 +15,16 @@ def print_env(
     print("-" * 9)
     print(OUTPUT_STREAM)
 
+load_dotenv()
 
 
-REDIS_DUPLICATE_FILTER_KEY: Optional[str] = "ingestor:seen.articles"
+REDIS_DUPLICATE_FILTER_KEY: Optional[str] = os.getenv("REDIS_DUPLICATE_FILTER_KEY")
 if not REDIS_DUPLICATE_FILTER_KEY:
     print("FATAL: REDIS_DUPLICATE_FILTER_KEY environment variable is not set. Exiting.")
     sys.exit(1)
 
 
-OUTPUT_STREAM: Optional[str] = "ingestor:to.be.scraped"
+OUTPUT_STREAM: Optional[str] = os.getenv("OUTPUT_STREAM")
 if not OUTPUT_STREAM:
     print("FATAL: OUTPUT_STREAM environment variable is not set. Exiting.")
     sys.exit(1)
