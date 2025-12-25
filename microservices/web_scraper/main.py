0c5be331e9a793be9211cc386977924fcbe15704
bobblet11 <u3596276@connect.hku.hk>
2025-12-25 06:59:31 +0000

    feat: refactor main and config of web scraper

--- microservices/web_scraper/main.py
+++ microservices/web_scraper/main.py
@@ -1,20 +1,9 @@
-import datetime
 import signal
 
 from microservices.web_scraper.ScraperService import ScraperService
 
-
-def exec():
+if __name__ == "__main__":
     scraper_service = ScraperService()
-
     signal.signal(signal.SIGINT, scraper_service.shutdown)
     signal.signal(signal.SIGTERM, scraper_service.shutdown)
-
     scraper_service.run()
-
-
-if __name__ == "__main__":
-
-    print(f"\n\nmain.py is being run. It is currently {datetime.datetime.now()}")
-    exec()
-    print(f"\n\nmain.py is finished. It is currently {datetime.datetime.now()}")
