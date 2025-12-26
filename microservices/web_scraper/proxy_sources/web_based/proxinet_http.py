ead8dc3684a1d5c240257c103eb4a9d27ae9c219
bobblet11 <u3596276@connect.hku.hk>
2025-12-26 08:12:27 +0000

    refactored another proxy source

--- microservices/web_scraper/proxy_sources/web_based/proxinet_http.py
+++ microservices/web_scraper/proxy_sources/web_based/proxinet_http.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 from microservices.web_scraper.proxy_sources.base_classes import (
     HttpProxySource,
-    normalize_proxy_scheme,
+    ProxyUtils,
 )
 
 # --- Type Hinting for Clarity ---
@@ -15,39 +15,41 @@ ProxyRequestDict = Optional[Dict[str, str]]
 class ProxiNetHttpSource(HttpProxySource):
     """Scrapes HTTP/HTTPS proxies from free-proxy-list.net."""
 
-    URL = "https://free-proxy-list.net/"
+    URL:str = "https://free-proxy-list.net/"
 
     def _parse_html_table(self, html_content: str) -> List[str]:
         """Parses the HTML table from free-proxy-list.net."""
-        https_proxies: Set[str] = set()
+        
         try:
+            https_proxies: Set[str] = set()
             soup = BeautifulSoup(html_content, features="html.parser")
             tbody = soup.find("tbody")
+            
             if not tbody:
-                print(
-                    f"[{self.name}] [!] Could not find tbody. Site layout may have changed."
-                )
-                return []
+                raise Exception(f"Could not find tbody. Site layout may have changed.")
 
             rows = tbody.find_all("tr")
+            
             for row in rows:
                 cols = row.find_all("td")
                 if len(cols) >= 7 and cols[6].text.strip().lower() == "yes":
                     ip, port = cols[0], cols[1]
+                    
                     https_proxies.add(
-                        normalize_proxy_scheme(f"{ip}:{port}", scheme="http")
+                        ProxyUtils.normalize_scheme(f"{ip}:{port}", scheme="http")
                     )
 
+            return list(https_proxies)
+        
         except Exception as e:
-            print(f"[{self.name}] [!] Error parsing HTML: {e}")
-
-        return list(https_proxies)
+            self.logger.error(f"Could not parse proxinet HTML: {e}")
+            return []
+      
 
     def get_proxies(
         self, bootstrap_proxies: Optional[Dict[str, str]] = None
     ) -> Dict[str, List[str]]:
-        """Fetches HTTPS proxies by scraping the webpage."""
-
+        """Fetches HTTPS proxies by scraping the webpage. Proxinet only provides HTTPS proxies"""
         https_proxies = self._fetch_from_url(
             self.URL, bootstrap_proxies, self._parse_html_table
         )
