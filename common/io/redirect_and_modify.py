b881f79a77c5878cc5e1dabcf97dec9fd7f55ea3
bobblet11 <u3596276@connect.hku.hk>
2025-12-27 13:10:45 +0000

    refactored and using new logging  function

--- common/io/redirect_and_modify.py
+++ common/io/redirect_and_modify.py
@@ -1,28 +1,32 @@
 import io
 import sys
 from contextlib import redirect_stdout
-
+from typing import Callable, Tuple, Any, List
 from common.io.utils import indent_with_tab
 
 
-def redirect_and_modify(string_modification_function=indent_with_tab):
-    def decorator(func):
-        def wrapper(*args, **kwargs):
+def redirect_and_modify(string_modification_function: Callable=indent_with_tab) -> Callable:
+    def decorator(func: Callable) -> Callable:
+        def wrapper(*args: Tuple, **kwargs:Tuple):
             string_buffer = io.StringIO()
+            
             with redirect_stdout(string_buffer):
                 func(*args, **kwargs)
-            captured_output = string_buffer.getvalue()
+                
+            captured_output:str = string_buffer.getvalue()
             if not captured_output:
                 return
-            lines = captured_output.split("\n")
+            
+            lines:List[str] = captured_output.split("\n")
+            
             if lines[-1] == "":
-                modified_lines = [
+                modified_lines:List[str] = [
                     string_modification_function(line) for line in lines[:-1]
                 ]
-                final_output = "\n".join(modified_lines) + "\n"
+                final_output:str = "\n".join(modified_lines) + "\n"
             else:
-                modified_lines = [string_modification_function(line) for line in lines]
-                final_output = "\n".join(modified_lines)
+                modified_lines:List[str] = [string_modification_function(line) for line in lines]
+                final_output:str = "\n".join(modified_lines)
 
             sys.stdout.write(final_output)
 
