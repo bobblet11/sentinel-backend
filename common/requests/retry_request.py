de2764059eea94c0b1955dc4ce46c2310435445e
bobblet11 <u3596276@connect.hku.hk>
2025-12-27 13:12:08 +0000

    feat: using new logging functions + refactored

--- common/requests/retry_request.py
+++ common/requests/retry_request.py
@@ -2,25 +2,27 @@ import functools
 import time
 from math import exp
 from random import uniform
+from typing import Callable, Any
+from common.io.logging import get_logger
+from logging import getLogger, Logger
 
-
-def retry(max_attempts=3, delay_s=1):
-    def decorator_retry(func):
-        def wrapper(*args, **kwargs):
+def retry(max_attempts:int=3, delay_s:int=1) -> Callable:
+    def decorator_retry(func: Callable) -> Callable:
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs) -> Any:
+            logger:Logger = get_logger(func, args)
+            
             for attempt in range(1, max_attempts + 1):
                 try:
-                    print(f"Attempt #{attempt}")
+                    logger.debug(f"Attempt #{attempt}")
                     return func(*args, **kwargs)
+                
                 except Exception as e:
                     if attempt == max_attempts:
-                        print(f"Fatal failure at attempt {attempt}. Exiting...")
+                        logger.warning(f"Attempted all {max_attempts} attempts. Exiting...")
                         raise e
-
-                    print(
-                        f"Failure at attempt {attempt}. Will retry in {delay_s:.2f} s"
-                    )
+                    logger.warning(f"[Retry] Failure at attempt {attempt} doing '{func.__name__}' with error {type(e).__name__}. Will retry in {delay_s:.2f} s")
                     time.sleep(delay_s)
-
         return wrapper
 
     return decorator_retry
@@ -32,46 +34,37 @@ def exponential_retry(
     initial_delay_s: float = 1,
     growth_modifier: float = 1,
     growth_rate: float = 1,
-    jitter=False,
-):
-    def decorator_exponential_retry(func):
+    jitter:bool=False,
+) -> Callable:
+    def decorator_exponential_retry(func:Callable) -> Callable:
         @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            last_exception = None
-
+        def wrapper(*args, **kwargs) -> Any:
+            logger:Logger = get_logger(func, args)
+            last_exception:Exception = None
+            
             for attempt in range(1, max_attempts + 1):
                 try:
-                    print(f"Attempt #{attempt}")
+                    logger.debug(f"Attempt #{attempt}")
                     return func(*args, **kwargs)
 
-                # It now catches only the exceptions you specified. If exception not here, then it will raise
                 except on_exceptions as e:
                     last_exception = e
 
                     # If you reach here, then all attempts failed.
                     if attempt == max_attempts:
-                        print(f"Fatal failure at attempt {attempt}. Exiting...")
-                        # Will raise an exception
+                        logger.warning(f"Attempted all {max_attempts} attempts. Exiting...")
                         break
 
-                    time_to_wait_s = (
+                    time_to_wait_s: float = (
                         growth_modifier * exp(attempt * growth_rate)
                     ) + initial_delay_s
 
                     if jitter and time_to_wait_s > 1:
                         time_to_wait_s = uniform(time_to_wait_s - 1, time_to_wait_s + 1)
-
-                    print(
-                        f"[Retry] Attempt {attempt} failed for '{func.__name__}' with {type(e).__name__}. Retrying in {time_to_wait_s:.2f}s..."
-                    )
-
+                        
+                    logger.warning(f"[Retry] Failure at attempt {attempt} doing '{func.__name__}' with error {type(e).__name__}. Will retry in {time_to_wait_s:.2f} s")
                     time.sleep(time_to_wait_s)
-
-            print(
-                f"[Retry] All {max_attempts} attempts for '{func.__name__}' failed. Raising final error."
-            )
             raise last_exception
-
         return wrapper
 
     return decorator_exponential_retry
