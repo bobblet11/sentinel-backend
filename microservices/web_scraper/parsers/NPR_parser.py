f3e844904038a5919580f45365b8135edfa4e31f
bobblet11 <u3596276@connect.hku.hk>
2025-12-28 10:39:26 +0000

    feat: refactored hardcoded parser logic.

---
+++ microservices/web_scraper/parsers/NPR_parser.py
@@ -0,0 +1,20 @@
+# nyt_scraper.py
+import re
+from typing import Dict, Optional
+from urllib.parse import urlparse
+
+from bs4 import BeautifulSoup
+
+from microservices.web_scraper.parsers.base_parser import BaseParser
+
+
+class NPRParser(BaseParser):
+    def extract(self, soup: BeautifulSoup, article_url: str) -> Optional[Dict]:
+        return
+
+        return {
+            "title": title,
+            "text": text,
+            "author": author,
+            "published_at": published_at,
+        }
