85fdb212b550ffc24393ab870e2a888d79c8a155
bobblet11 <u3596276@connect.hku.hk>
2025-12-29 22:33:47 +0000

    feat: - separated db and db_wrapper_api - added connection from db_wrapper_api to db -

---
+++ microservices/db_wrapper_api/src/setup/connect_to_postgres.py
@@ -0,0 +1,25 @@
+from logging import Logger, getLogger
+from fastapi import FastAPI
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+from microservices.db_wrapper_api.config import POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_DB, POSTGRES_PORT
+
+
+
+logger:Logger = getLogger("connection_to_postgres")
+
+# We should replace localhost with our Docker host IP if we connect from a different machine.
+# connection string format
+# postgresql://[user[:password]@][host][:port][/dbname][?param1=value1&param2=value2]
+database_url:str = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+engine = create_engine(database_url)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+   
+def get_db() -> Session:
+	logger.info("Attemping to connect to postgres database")
+	db = SessionLocal()
+	logger.info("Successfully connected")
+	try:
+		yield db
+	finally:
+		db.close()
