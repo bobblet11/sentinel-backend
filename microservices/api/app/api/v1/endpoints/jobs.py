f0692ec0468af3298a537b4453b2091320a4c457
bobblet11 <u3596276@connect.hku.hk>
2026-01-16 07:58:36 +0000

    feat: generic job creation endpoint

--- microservices/api/app/api/v1/endpoints/jobs.py
+++ microservices/api/app/api/v1/endpoints/jobs.py
@@ -1,16 +1,29 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 from microservices.api.app.db.session import get_db
 from microservices.api.app.dtos.job import JobCreate, JobResponse
-from microservices.api.app.crud import crud_job
+from microservices.api.app.crud.crud_job import create_job, get_job
 from microservices.api.app.services import redis_queue
 from sqlalchemy.orm import Session
 
 router = APIRouter()
 
-@router.post("/", response_model=JobResponse)
+@router.post("/", response_model=JobResponse, status_code=status.HTTP_202_ACCEPTED)
 def submit_job(job_in: JobCreate, db: Session = Depends(get_db)):
-	pass
+        new_job: JobResponse = create_job(db=db, job_in=job_in)
+        
+        # redis_queue.enqueue_job(job_id=new_job.id, article_url=new_job.article_url)
+        
+        return new_job
 
-@router.get("/{job_id}", response_model=JobResponse)
+
+@router.get("/{job_id}", response_model=JobResponse, status_code=status.HTTP_200_OK)
 def read_job_status(job_id: str, db: Session = Depends(get_db)):
-	pass
+    job = get_job(db=db, job_id=job_id)
+
+    if job is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Job with ID {job_id} not found."
+        )
+    
+    return job
--- microservices/api/app/crud/crud_job.py
+++ microservices/api/app/crud/crud_job.py
@@ -1,20 +1,26 @@
 from sqlalchemy.orm import Session
-from microservices.api.app.models.job import JobRequest
-from microservices.api.app.dtos.job import JobCreate
+from microservices.api.app.models.job import Job
+from microservices.api.app.dtos.job import JobCreate, JobResponse, JobStatus, JobType
 
-def create_job(db: Session, job: JobCreate):
-    db_obj = JobRequest(
-        user_id=job.user_id,
-        input_payload=job.input_payload,
-        status="PENDING"
-    )
-    db.add(db_obj)
-    db.commit()
-    db.refresh(db_obj)
-    return db_obj
+def create_job(db: Session, job_dto: JobCreate)->JobResponse:
+    try:
+        db_obj = Job(
+            type = JobType.USER,
+            status = JobStatus.PENDING
+        )
+        # use job_dto to add article entry as well and link to job
+
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
+    except Exception as e:
+        db.rollback()
+
 
 def get_job(db: Session, job_id: str):
-    return db.query(JobRequest).filter(JobRequest.id == job_id).first()
+    return db.query(Job).filter(Job.id == job_id).first()
+
 
 def generate_db_statistics(db: Session):
     pass
