26b791bfd10a53ea5373fe7b9344d689d3146ab8
bobblet11 <u3596276@connect.hku.hk>
2026-01-20 16:56:54 +0000

    switched to benchmark

--- microservices/web_scraper/main.py
+++ microservices/web_scraper/main.py
@@ -17,15 +17,16 @@ from microservices.web_scraper.config import (
     SCRAPER_MAX_WORKERS,
     USER_OUTPUT_STREAM
 )
+from microservices.web_scraper.scraper_service_benchmark import ScraperServiceBenchmark
 SERVICE_NAME="scraper"
 CONTAINER_NAME="web_scraper"
 
 if __name__ == "__main__":
     setup_logging(level=DEBUG,container_name=CONTAINER_NAME)
-    routing_map = {"user": USER_OUTPUT_STREAM, "background": BACKGROUND_OUTPUT_STREAM}
+    routing_map = {JobType.USER.value: USER_OUTPUT_STREAM, JobType.BACKGROUND.value: BACKGROUND_OUTPUT_STREAM}
     
-    config = ServiceConfig(max_workers=SCRAPER_MAX_WORKERS, BATCH_SIZE=BATCH_SIZE,service_name=SERVICE_NAME, input_streams=[INPUT_STREAM], group_name=GROUP_NAME, CONSUMER_NAME=CONSUMER_NAME, failure_output_stream=FAILURE_OUTPUT_STREAM, routing_map=routing_map,is_concurrent=True, batch_size=BATCH_SIZE )
-    scraper_service = ScraperService(config)
+    config = ServiceConfig(max_workers=SCRAPER_MAX_WORKERS, service_name=SERVICE_NAME, input_streams=[INPUT_STREAM], group_name=GROUP_NAME, consumer_name=CONSUMER_NAME, failure_output_stream=FAILURE_OUTPUT_STREAM, routing_map=routing_map,is_concurrent=True, batch_size=BATCH_SIZE )
+    scraper_service = ScraperServiceBenchmark(config)
     signal.signal(signal.SIGINT, scraper_service.shutdown)
     signal.signal(signal.SIGTERM, scraper_service.shutdown)
     scraper_service.run()
