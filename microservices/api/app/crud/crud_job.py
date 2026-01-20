bbb5cb3954dad0308bc3bd3160896d6093c59f43
bobblet11 <u3596276@connect.hku.hk>
2026-01-20 13:05:01 +0000

    feat: api uses new sql db. redis not implemented

--- microservices/api/app/crud/crud_job.py
+++ microservices/api/app/crud/crud_job.py
@@ -1,22 +1,26 @@
+import uuid
 from sqlalchemy.orm import Session
 from microservices.api.app.models.job import Job
 from microservices.api.app.dtos.job import JobCreate, JobResponse, JobStatus, JobType
+from microservices.api.app.core.logger import logger
 
-def create_job(db: Session, job_dto: JobCreate)->JobResponse:
-    try:
-        db_obj = Job(
-            type = JobType.USER,
-            status = JobStatus.PENDING
-        )
-        # use job_dto to add article entry as well and link to job
-        
+def create_job(db: Session, job_in: JobCreate, article_id: int)->Job:
+    uid:str = str(uuid.uuid4())
+    logger.info(article_id)
     
-        db.add(db_obj)
-        db.commit()
-        db.refresh(db_obj)
-        return db_obj
-    except Exception as e:
-        db.rollback()
+    db_obj = Job(
+        uid = uid,
+        article_id = article_id,
+        type = JobType.USER,
+        status = JobStatus.PENDING
+    )
+
+
+    db.add(db_obj)
+    db.flush()
+    db.refresh(db_obj)
+    logger.info(f"Prepared job for creation: {db_obj}")
+    return db_obj
 
 
 def get_job(db: Session, job_id: str):
