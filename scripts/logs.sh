2598dc361b95d15cbefef9c99dbc4c59582c9257
PorpanTH <60740681+PorpanTH@users.noreply.github.com>
2026-01-21 04:10:31 +0000

    api container running accessing postgres

--- scripts/logs.sh
+++ scripts/logs.sh
@@ -7,4 +7,8 @@ set -u
 # Pipes will fail if any command in the pipe fails, not just the last one.
 set -o pipefail
 
+# Navigate to the project root
+PROJECT_ROOT=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")/.." &> /dev/null && pwd)
+cd "$PROJECT_ROOT/docker/compose"
+
 sudo docker-compose logs -f
