378fccb8e0cecff4f7c0e3b10c20a5d6e1b7207a
bobblet11 <u3596276@connect.hku.hk>
2026-01-21 07:52:18 +0000

    refactor

--- microservices/web_scraper/scraper_service_benchmark.py
+++ microservices/web_scraper/scraper_service_benchmark.py
@@ -2,6 +2,7 @@ import json
 import os
 from pathlib import Path
 from typing import Any, Dict, Tuple
+from common.io.save_message import MessageSaver
 from common.models.api.redis_models import StreamMessage
 from common.service.service_template import ProcessingError, ServiceConfig
 from microservices.web_scraper.scraper_service import ScraperService
@@ -27,41 +28,16 @@ class FailedToParse(Exception):
 class ScraperServiceBenchmark(ScraperService):
     """Concurrently scrapes, parses, and publishes messages"""
 
-    def __init__(self,config: ServiceConfig, log_directory:Path=Path("/app/logs")):
+    def __init__(self,config: ServiceConfig, message_store_filename: str = "messages.json", log_directory:Path=Path("/app/logs")):
         super().__init__(config)  
-        message_store_filename:str = f"messages.json"
-    
-        #create file
-        if isinstance(log_directory, str):
-            log_directory = Path(log_directory)
-        log_directory.mkdir(mode=777,parents=True, exist_ok=True)
-        message_store_filepath:Path = log_directory / message_store_filename
-        message_store_filepath.touch(mode=777, exist_ok=True)
-        os.chmod(str(message_store_filepath), 0o666)
-        self.message_store_filepath: Path = message_store_filepath
-        
-        with open(str(self.message_store_filepath), "r+") as file:
-            json.dump({"messages":[]} , file, indent=4)
+        self.message_saver = MessageSaver(message_store_filename, log_directory)
 
 
     def _process_message(self, message: StreamMessage) -> StreamMessage:
         try:
             fetched_message:StreamMessage = self._fetch_article_and_update(message)
             parsed_message:StreamMessage = self._parse_article_and_update(fetched_message)
-            new_message:Dict[str,Any] = parsed_message.data.model_dump()
-
-            try:
-                with open(str(self.message_store_filepath), "r") as file:
-                    file_data = json.load(file)
-            except (json.JSONDecodeError, FileNotFoundError):
-    
-                file_data = {"messages": []}
-
-            file_data["messages"].append(new_message)
-
-            with open(str(self.message_store_filepath), "w") as file:
-                json.dump(file_data, file, indent=4)
-                
+            self.message_saver.save_new_message(parsed_message)
             return parsed_message
         
         except FailedToFetch as e:
@@ -71,7 +47,7 @@ class ScraperServiceBenchmark(ScraperService):
             raise ProcessingError(f"Failed to parse {message.link}: {e}")
    
     def _process_and_publish_worker(self, message: StreamMessage) -> Tuple[str, str]:
-        """Worker for concurrent mode. Processes, then publishes."""
+        #Ack but dont publish
         try:
             self._process_message(message)
             self.message_consumer.acknowledge(message.stream, message.redis_id)
