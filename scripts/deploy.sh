d605446ce2569b3eb6950d551d3dad80ebce5678
PorpanTH <60740681+PorpanTH@users.noreply.github.com>
2026-03-10 07:39:04 +0000

    ARM support

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -185,8 +185,13 @@ run_and_log sudo -E docker build --pull=false -t sentinel/python-light-common:3.
 log_warn "Building base Docker image \"CPU_ML_base\"..."
 run_and_log sudo -E docker build --pull=false -t sentinel/python-ml-cpu:3.12 -f "$PROJECT_ROOT/docker/base/CPU_ML_base/Dockerfile" "$PROJECT_ROOT"
 
-log_warn "Building base Docker image \"GPU_ML_base\"..."
-run_and_log sudo -E docker build --pull -t sentinel/python-ml-gpu:3.12-cuda121 -f "$PROJECT_ROOT/docker/base/GPU_ML_base/Dockerfile" "$PROJECT_ROOT"  
+ARCH=$(uname -m)
+if [ "$ARCH" = "x86_64" ]; then
+    log_warn "Building base Docker image \"GPU_ML_base\"..."
+    run_and_log sudo -E docker build --pull -t sentinel/python-ml-gpu:3.12-cuda121 -f "$PROJECT_ROOT/docker/base/GPU_ML_base/Dockerfile" "$PROJECT_ROOT"
+else
+    log_warn "Skipping GPU_ML_base build on unsupported architecture: $ARCH"
+fi
 
 
 log_info "Building service images for '$CONFIG_NAME' configuration..."
