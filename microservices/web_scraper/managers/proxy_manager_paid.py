b0915ed081aecddc05115153dba22e0d25e6e155
bobblet11 <u3596276@connect.hku.hk>
2026-04-10 20:19:13 +0000

    fix: added lock to get proxy to prevent race condition

--- microservices/web_scraper/managers/proxy_manager_paid.py
+++ microservices/web_scraper/managers/proxy_manager_paid.py
@@ -2,6 +2,7 @@ import random
 import threading
 from typing import Dict, List, Optional, Set
 from logging import getLogger, Logger
+from common.requests.retry_request import exponential_retry
 from microservices.web_scraper.proxy_sources.web_based.webshareio_http import (
     WebshareIOHttpSource,
 )
@@ -43,20 +44,33 @@ class ProxyManagerPaid:
             # --- State ---
             self.proxies:ProxyDictSet = {"https": set(), "socks4": set(), "socks5": set()}
             self.rotate_index: int = 0
-
+            self.fail_count:int = 0
+            self.acceptable_failure_rate: float = 0.1
+            
             # --- Concurrency ---
             self._refresh_lock = threading.Lock()
 
             # --- Dependency Injection for Sources ---
             self.webshareio_http_source = WebshareIOHttpSource(
-                "WebshareIoHttp", timeout=(20.0, 22.0)
+                "WebshareIoHttp", timeout=(60.0, 60.0)
             )
             
-            self._init_proxies()
+            try:
+                self._init_proxies()
+            except Exception as e:
+                self.logger.error(f"Failed to load proxies: {e}")
+                raise
             
             self._initialized = True
             self.logger.info(f"Initialisation complete!")
 
+    @exponential_retry(
+        max_attempts=3,
+        initial_delay_s=10,
+        on_exceptions=(
+            Exception
+        ),
+    )
     def _init_proxies(self):
             self.proxies["https"].update(self.webshareio_http_source.get_proxies()["https"])
             self.ip_country_mapping: Dict[str, str] = (
@@ -89,24 +103,29 @@ class ProxyManagerPaid:
         """
         Public method to get a proxy and rotate to next proxy.
         """
-        all_usable_proxies:Set[str] = self._get_all_usable_proxies()
-        if not all_usable_proxies:
-            raise Exception("No usable proxies available!")
+        with self._refresh_lock:
+            all_usable_proxies:Set[str] = self._get_all_usable_proxies()
+            if not all_usable_proxies:
+                raise Exception("No usable proxies available!")
 
-        # Should not use US proxy for BBC since they have stricter paywall
-        if "bbc" in article_url:
-            index: int = self.rotate_index % len(self.country_ip_mapping)
-            chosen_british_proxy: str = self.country_ip_mapping["GB"][index]
-            return self._create_proxy_dict(chosen_british_proxy)
+            # Should not use US proxy for BBC since they have stricter paywall
+            if "bbc" in article_url:
+                index: int = self.rotate_index % len(self.country_ip_mapping)
+                chosen_british_proxy: str = self.country_ip_mapping["GB"][index]
+                return self._create_proxy_dict(chosen_british_proxy)
 
-        chosen_global_proxy = list(all_usable_proxies)[self.rotate_index]
-        self.rotate_index = (self.rotate_index + 1) % len(all_usable_proxies)
-        return self._create_proxy_dict(chosen_global_proxy)
+            chosen_global_proxy = list(all_usable_proxies)[self.rotate_index]
+            self.rotate_index = (self.rotate_index + 1) % len(all_usable_proxies)
+            return self._create_proxy_dict(chosen_global_proxy)
 
     def report_bad_proxy(self, proxy_url: str) -> None:
         """Does not need to do anything for paid proxies"""
-        self.logger.warning(f"proxy {proxy_url} failed, refreshing proxies...")
-        self.reset()
+        self.logger.warning(f"proxy {proxy_url} failed")
+        self.fail_count += 1
+        
+        if self.fail_count > self.acceptable_failure_rate * len(self._get_all_usable_proxies()):
+            self.reset()
+            self.fail_count = 0
         return
 
     def _get_all_usable_proxies(self) -> Set[str]:
